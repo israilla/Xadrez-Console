809332c48c625870d8add2cd7a9e5b85ff871f9a
Israilla <israillacaroline@hotmail.com>
2020-05-04 02:37:31 -0300

    Implementacao do tabuleiro iniciada

---
+++ Xadrez-console/Tabuleiro/Peca.cs
@@ -0,0 +1,20 @@
+﻿using tabuleiro;
+
+namespace tabuleiro
+{
+    class Peca
+    {
+        public Posicao Posicao { get; set; }
+        public Cor Cor { get; protected set; }
+        public int QtdeMovimentos{get;protected set ;}
+        public Tabuleiro Tab { get; protected set; }
+
+        public Peca(Posicao posicao, Cor cor, int qtdeMovimentos, Tabuleiro tab)
+        {
+            Posicao = posicao;
+            Cor = cor;
+            QtdeMovimentos = qtdeMovimentos;
+            Tab = tab;
+        }
+    }
+}
