4b8e270f22dd4be68d779ff5d531cb7d6971caf3
Israilla <israillacaroline@hotmail.com>
2020-05-07 14:30:45 -0300

    Jogada especial promoção

--- Xadrez-console/xadrez/PartidaDeXadrez.cs
+++ Xadrez-console/xadrez/PartidaDeXadrez.cs
@@ -137,6 +137,20 @@ namespace xadrez
                 DesfazMovimento(origem, destino, pecaCapturada);
                 throw new TabuleiroException("Você não pode se colocar em xeque!");
             }
+            Peca p = Tab.Peca(destino);
+
+            //#JogadaEspecial Promoção
+            if (p is Peao)
+            {
+                if ((p.Cor== Cor.Branca && destino.Linha==0)|| (p.Cor == Cor.Preta && destino.Linha == 7))
+                {
+                    p = Tab.RetirarPeca(destino);
+                    Pecas.Remove(p);
+                    Peca dama = new Dama(Tab, p.Cor);
+                    Tab.ColocarPeca(dama, destino);
+                    Pecas.Add(dama);
+                }
+            }
 
             if (EstaEmXeque(Adversaria(JogadorAtual)))
             {
@@ -155,9 +169,6 @@ namespace xadrez
                 Turno++;
                 MudaJogador();
             }
-
-            Peca p = Tab.Peca(destino);
-
             //#JogadaEspecial En Passant
             if (p is Peao && (destino.Linha == origem.Linha - 2 || destino.Linha == origem.Linha + 2))
             {
