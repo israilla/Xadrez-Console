18b718d09470fd3658c2897456b18e046f917ea1
Israilla <israillacaroline@hotmail.com>
2020-05-06 21:50:25 -0300

    Criação das demais classes das demais peças e suas respectivas posições no tabuleiro

--- Xadrez-console/Program.cs
+++ Xadrez-console/Program.cs
@@ -13,36 +13,37 @@ namespace Xadrez_console
                 PartidaDeXadrez partida = new PartidaDeXadrez();
 
                 while (!partida.Terminada)
-
+                {
                     try
                     {
-                        {
-                            Console.Clear();
-                            Tela.ImprimirPartida(partida);
+                        Console.Clear();
+                        Tela.ImprimirPartida(partida);
 
-                            Console.WriteLine();
-                            Console.Write("Origem: ");
-                            Posicao origem = Tela.LerPosicaoXadrez().ToPosicao();
-                            partida.ValidarPosicaoDeOrigem(origem);
+                        Console.WriteLine();
+                        Console.Write("Origem: ");
+                        Posicao origem = Tela.LerPosicaoXadrez().ToPosicao();
+                        partida.ValidarPosicaoDeOrigem(origem);
 
-                            bool[,] posicoesPossiveis = partida.Tab.Peca(origem).MovimentosPossiveis();
+                        bool[,] posicoesPossiveis = partida.Tab.Peca(origem).MovimentosPossiveis();
 
-                            Console.Clear();
-                            Tela.ImprimirTabuleiro(partida.Tab, posicoesPossiveis);
+                        Console.Clear();
+                        Tela.ImprimirTabuleiro(partida.Tab, posicoesPossiveis);
 
-                            Console.WriteLine();
-                            Console.Write("Destino: ");
-                            Posicao destino = Tela.LerPosicaoXadrez().ToPosicao();
-                            partida.ValidarPosicaoDeDestino(origem, destino);
+                        Console.WriteLine();
+                        Console.Write("Destino: ");
+                        Posicao destino = Tela.LerPosicaoXadrez().ToPosicao();
+                        partida.ValidarPosicaoDeDestino(origem, destino);
 
-                            partida.RealizaJogada(origem, destino);
-                        }
+                        partida.RealizaJogada(origem, destino);
                     }
                     catch (TabuleiroException e)
                     {
                         Console.WriteLine(e.Message);
                         Console.ReadLine();
                     }
+                }
+                Console.Clear();
+                Tela.ImprimirPartida(partida);
             }
             catch (TabuleiroException e)
             {
--- Xadrez-console/Tela.cs
+++ Xadrez-console/Tela.cs
@@ -14,11 +14,19 @@ namespace Xadrez_console
             ImprimirPecasCapturadas(partida);
             Console.WriteLine();
             Console.WriteLine("Turno: " + partida.Turno);
-            if (partida.Xeque)
+            if (!partida.Terminada)
             {
-                Console.WriteLine("XEQUE!");
+                Console.WriteLine("Aguardando jogada: " + partida.JogadorAtual);
+                if (partida.Xeque)
+                {
+                    Console.WriteLine("XEQUE!");
+                }
             }
-            Console.WriteLine("Aguardando jogada: " + partida.JogadorAtual);
+            else
+            {
+                Console.WriteLine("XEQUEMATE!");
+                Console.WriteLine("Vencedor: " + partida.JogadorAtual);
+            }
         }
 
         public static void ImprimirPecasCapturadas (PartidaDeXadrez partida)
